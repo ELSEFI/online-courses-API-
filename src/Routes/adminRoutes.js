d5dd5c40a993177be7aa1478c70aa9bf6d60d9ce
ELSEFI <mohamedelsafy.as@gmail.com>
2025-12-06 21:32:58 +0200

    Add Admin Accept For Be Instructor and Rejected

--- src/Routes/adminRoutes.js
+++ src/Routes/adminRoutes.js
@@ -4,19 +4,17 @@ const protected = require("../Middleware/jwtMiddleware");
 const restrictTo = require("../Middleware/roleMiddleware");
 
 const router = express.Router();
+router.use(protected, restrictTo("admin"));
 
-router.get(
-  "/users/requests",
-  protected,
-  restrictTo("admin"),
-  adminController.getAllRequests
+router.get("/users/requests", adminController.getAllRequests);
+router.get("/users/requests/:requestId", adminController.getRequest);
+router.patch(
+  "/users/requests/:requestId/approve-request",
+  adminController.acceptInstructor
 );
-
-router.get(
-  "/users/requests/:id",
-  protected,
-  restrictTo("admin"),
-  adminController.getRequest
+router.patch(
+  "/users/requests/:requestId/reject-request",
+  adminController.rejectInstructor
 );
 
 module.exports = router;
--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -4,9 +4,12 @@ const User = require("../models/User");
 
 exports.getAllRequests = async (req, res) => {
   try {
-    const requests = await instructorRequest.find();
+    const requests = await instructorRequest
+      .find()
+      .populate("userId", "name email profileImage")
+      .sort({ createdAt: -1 });
     if (requests.length === 0)
-      return res.status(400).json({ message: "No Requests Found" });
+      return res.status(404).json({ message: "No Requests Found" });
 
     res.status(200).json(requests);
   } catch (error) {
@@ -20,9 +23,14 @@ exports.getRequest = async (req, res) => {
     const { requestId } = req.params;
     const request = await instructorRequest.findById(requestId);
 
-    if (!request) return res.status(400).json({ message: "Request No Found!" });
+    if (!request) return res.status(404).json({ message: "Request No Found!" });
     await request.populate("userId", "name email profileImage");
-    res.status(200).json(request);
+    res.status(200).json({
+      request: {
+        ...request.toObject(),
+        cvURL: `${req.protocol}://${req.get("host")}/cvs/${request.cvFile}`,
+      },
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server Error" });
@@ -43,9 +51,9 @@ exports.acceptInstructor = async (req, res) => {
         .json({ message: "That Request Already Processed" });
 
     request.status = "approved";
-    await instructorRequest.save();
+    await request.save();
 
-    const instructorProfile = await instructorProfile.create({
+    const instructor = await instructorProfile.create({
       userId: request.userId,
       bio: {
         en: request.bio.en,
@@ -62,7 +70,42 @@ exports.acceptInstructor = async (req, res) => {
 
     res.status(201).json({
       message: "Instructor approved successfully",
-      profile: instructorProfile,
+      profile: instructor,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.rejectInstructor = async (req, res) => {
+  try {
+    const { requestId } = req.params;
+    const request = await instructorRequest.findById(requestId);
+    const { rejectionReason_en, rejectionReason_ar } = req.body;
+
+    if (!rejectionReason_en || !rejectionReason_ar) {
+      return res.status(404).json({
+        message: "Rejection reason is required in both languages",
+      });
+    }
+
+    if (!request)
+      return res.status(404).json({ message: "No Request With That Id" });
+
+    if (request.status !== "pending")
+      return res
+        .status(400)
+        .json({ message: "That Request Already Processed" });
+
+    request.status = "rejected";
+    request.rejectionReason.en = rejectionReason_en;
+    request.rejectionReason.ar = rejectionReason_ar;
+    await request.save();
+
+    res.status(201).json({
+      message: "Instructor rejected",
+      profile: request,
     });
   } catch (error) {
     console.error(error);
